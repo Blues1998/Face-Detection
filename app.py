a6c868e3608e65231749c8b226a42586e4371068
animesh98388 <animesh98388@gmail.com>
2020-08-14 04:23:40 +0530

    added sunglasses

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ https://sites.google.com/site/5kk73gpu2012/assignment/viola-jones-face-detection
 
 
 def main():
-    st.title("Face Detection App :sunglasses:")
+    st.title("Face Detection App :sunglasses: ")
     st.write("**Using the Haar cascade Classifiers**")
 
     activities = ["Home", "About"]
